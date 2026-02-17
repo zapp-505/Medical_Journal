82462b74590e856412701851792930e36d500bec
Harikesh <harikeshreji@gmail.com>
2026-02-17 18:58:14 +0530

    table creation line added

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from Website import create_app
+from Website import create_app, db
 import os
 
+
 app = create_app()
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
