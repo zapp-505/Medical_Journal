651de48866cd0e9333a446cc35ccf2c1b9896aed
Harikesh <harikeshreji@gmail.com>
2026-02-18 06:56:46 +0530

    fixed deployment errors

--- app.py
+++ app.py
@@ -1,11 +1,8 @@
-from Website import create_app, db
+from Website import create_app
 import os
 
 
 app = create_app()
-
-with app.app_context():
-    db.create_all()
 
 if __name__ == '__main__':
     app.run(debug=True)
